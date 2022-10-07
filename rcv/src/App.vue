3c9ad08a4e2d2d1805b38f3121ef6776b42e1e41
Brian DeRocher <brian.derocher@noblis.org>
2022-10-07 16:55:44 -0400

    Add vuetify.
    diff --git a/rcv/package-lock.json b/rcv/package-lock.json
    diff --git a/rcv/package.json b/rcv/package.json
    diff --git a/rcv/src/App.vue b/rcv/src/App.vue
    diff --git a/rcv/src/components/HelloWorld.vue b/rcv/src/components/HelloWorld.vue
    diff --git a/rcv/src/main.ts b/rcv/src/main.ts
    diff --git a/rcv/src/style.css b/rcv/src/style.css

--- rcv/src/App.vue
+++ rcv/src/App.vue
@@ -5,18 +5,25 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-main>
+      <v-container class="container">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+        <HelloWorld msg="Rank Choice Voting" />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
+.container {
+  background-color: rgb(150, 94, 255)
+}
 .logo {
   height: 6em;
   padding: 1.5em;
--- rcv/src/main.ts
+++ rcv/src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
+import 'vuetify/styles'
 import './style.css'
+import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const vuetify = createVuetify({ components, directives })
+
+app.use(vuetify)
+
+app.mount('#app')
